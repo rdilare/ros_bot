0a958a00cb4bd71cfd8a00f4730f28913e785cdb
Rahul Dilare <rdandroidunt@gmail.com>
2019-12-09 17:55:42 +0530

    tyr to make bot move smoother

--- src/Arduino_node.py
+++ src/Arduino_node.py
@@ -7,10 +7,38 @@ from serial import Serial
 from geometry_msgs.msg import Twist
 s = Serial("/dev/ttyUSB0",115200,timeout=0.1)
 
+
+pre_time = 0
+pre_v = 0
+pre_w = 0
+a_max = 1.5
+alpha_max = 1.2
+
+
+
 def runBot(msg):
+	global pre_time, pre_v, pre_w, a_max, alpha_max
+	curr_time = rospy.Time().now().to_sec()
+	delta_t = curr_time - pre_time
+	pre_time = curr_time
+	
 	v=msg.linear.x
 	w=msg.angular.z
-	l,r=.8,.3 #bot's track-width and wheel radius.
+	
+	#delta_v = (v-pre_v) if (v-pre_v)!=0 else 1
+	#delta_w = (w-pre_w) if (w-pre_w)!=0 else 1
+	
+	dir_v = (v-pre_v)/abs(v-pre_v) if (v-pre_v)!=0 else 0
+	dir_w = (w-pre_w)/abs(w-pre_w) if (w-pre_w)!=0 else 0
+	
+	calc_v = pre_v + dir_v*a_max*delta_t
+	calc_w = pre_w + dir_w*alpha_max*delta_t
+	
+	v = pre_v = calc_v if v!=0 else 0
+	w = pre_w = calc_w if w!=0 else 0
+	
+	
+	l,r=.14,.06 #bot's track-width and wheel radius.
 	vB = (1/r)*(v + (w*l/2))
 	vA = (1/r)*(v - (w*l/2))
 	global s
